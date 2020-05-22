172404258bf4ecfb6e65a054f3686334b781490f
Alexandreffaria <alexandreffaria@me.com>
2020-05-22 03:33:25 -0300

    opcao sharp - stripMetadata: false -> true

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,43 @@
 module.exports = {
-  plugins: [
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    },
-    {
-      resolve: 'gatsby-source-cosmicjs',
-      options: {
-        bucketSlug: 'blog-gcf',
-        objectTypes: ['posts','settings'],
-        apiAccess: {
-          read_key: 'laoHlsxw9a8Ht0zo8y4gZBNBPa05geItopgyNQYeLSDw3DKGI2',
+    plugins: [
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-plugin-sharp`,
+            options: {
+                stripMetadata: true,
+            },
         },
-        localMedia: true
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography',
-      },
-    },
-  ],
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/pages`,
+                name: 'pages',
+            },
+        },
+        {
+            resolve: 'gatsby-source-cosmicjs',
+            options: {
+                bucketSlug: 'blog-gcf',
+                objectTypes: ['posts', 'settings'],
+                apiAccess: {
+                    read_key: 'laoHlsxw9a8Ht0zo8y4gZBNBPa05geItopgyNQYeLSDw3DKGI2',
+                },
+                localMedia: true
+            }
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                //trackingId: `ADD YOUR TRACKING ID HERE`,
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-typography',
+            options: {
+                pathToConfigModule: 'src/utils/typography',
+            },
+        },
+    ],
 }
