d150ea39db197f4101045e0c68b5be24eed4a980
Alexandreffaria <alexandreffaria@me.com>
2020-05-22 11:17:09 -0300

    remocao do header da pagina layout

---
+++ src/components/header.js
@@ -0,0 +1,104 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import BackgroundImage from 'gatsby-background-image'
+import Img from 'gatsby-image'
+import { Link } from 'gatsby'
+import { rhythm, scale } from '../utils/typography'
+import { StaticQuery, graphql } from 'gatsby'
+
+export default () => ( 
+  <StaticQuery query = { graphql `
+    query LayoutQuery {
+      cosmicjsSettings(slug: { eq: "general" }) {
+        metadata {
+          site_heading
+          homepage_hero {
+            local {
+              childImageSharp {
+                fluid(
+                  quality: 90, 
+                  maxWidth: 1920,
+                  duotone: {
+                    highlight: "#f0960e",
+                    shadow: "#0e77f0",
+                    opacity: 61
+                  }
+                ){
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+          logodda {
+            local {
+              childImageSharp{
+                fluid(quality: 90, maxWidth: 300,){
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `}
+    render = {
+      data => {
+        const siteTitle = data.cosmicjsSettings.metadata.site_heading
+        const homgePageHero = data.cosmicjsSettings.metadata.homepage_hero.local.childImageSharp.fluid
+        const logoDDA = data.cosmicjsSettings.metadata.logodda.local.childImageSharp.fluid
+        return ( 
+          <div>
+            <BackgroundImage 
+                    Tag = "div"
+                    className = "post-hero"
+                    fluid = { homgePageHero }
+                    style = {{
+                        height: rhythm(24),
+                        position: 'relative',
+                        marginBottom: `${rhythm(1.1618)}`,
+                    }}
+                >
+                    <div 
+                        className = 'logo'
+                        style = {{
+                            maxWidth: 150,
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
+                            paddingTop: `${rhythm(1.1618)}`,
+                        }}
+                    >
+                        <Img 
+                            fluid = { logoDDA }
+                            alt = 'Logomarca Dia do Arauto'
+                        /> 
+                    </div> 
+                    <h1
+                        style = {{
+                            ...scale(1.3),
+                                position: 'absolute',
+                                textAlign: 'center',
+                                left: 0,
+                                right: 0,
+                                top: rhythm(10),
+                                marginTop: '0',
+                                height: rhythm(2.5),
+                                color: 'white'
+                        }}
+                    >
+                        <Link 
+                            style = {{
+                            boxShadow: 'none',
+                            textDecoration: 'none',
+                            color: 'inherit',
+                            }}
+                            to = { '/' } > { siteTitle } 
+                        </Link>
+                    </h1>
+                </BackgroundImage>
+          </div>
+        )
+      }
+    }
+  />
+)
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,107 +10,12 @@ import { rhythm, scale } from '../utils/typography'
 // Import typefaces
 import 'typeface-montserrat'
 import 'typeface-merriweather'
-import Head from './head.js'
+import Header from './header.js'
 
 export default ({ children, location }) => ( 
-  <StaticQuery query = { graphql `
-    query LayoutQuery {
-      cosmicjsSettings(slug: { eq: "general" }) {
-        metadata {
-          site_heading
-          homepage_hero {
-            local {
-              childImageSharp {
-                fluid(
-                  quality: 90, 
-                  maxWidth: 1920,
-                  duotone: {
-                    highlight: "#f0960e",
-                    shadow: "#0e77f0",
-                    opacity: 61
-                  }
-                ){
-                  ...GatsbyImageSharpFluid_withWebp
-                }
-              }
-            }
-          }
-          logodda {
-            local {
-              childImageSharp{
-                fluid(quality: 90, maxWidth: 300,){
-                  ...GatsbyImageSharpFluid_withWebp
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `}
-    render = {
-      data => {
-        const siteTitle = data.cosmicjsSettings.metadata.site_heading
-        const homgePageHero = data.cosmicjsSettings.metadata.homepage_hero.local.childImageSharp.fluid
-        const logoDDA = data.cosmicjsSettings.metadata.logodda.local.childImageSharp.fluid
-        let header
-        let rootPath = `/`
-        let postsPath = `/posts`
-            
-        header = ( 
-          <BackgroundImage 
-            Tag = "div"
-            className = "post-hero"
-            fluid = { homgePageHero }
-            style = {{
-              height: rhythm(24),
-              position: 'relative',
-              marginBottom: `${rhythm(1.1618)}`,
-            }}
-          >
-            <div 
-              className = 'logo'
-              style = {{
-                maxWidth: 150,
-                marginLeft: 'auto',
-                marginRight: 'auto',
-                paddingTop: `${rhythm(1.1618)}`,
-              }}
-            >
-              <Img 
-                fluid = { logoDDA }
-                alt = 'Logomarca Dia do Arauto'
-              /> 
-            </div> 
-            <h1
-              style = {{
-                ...scale(1.3),
-                  position: 'absolute',
-                  textAlign: 'center',
-                  left: 0,
-                  right: 0,
-                  top: rhythm(10),
-                  marginTop: '0',
-                  height: rhythm(2.5),
-                  color: 'white'
-              }}
-            >
-              <Link 
-                style = {{
-                  boxShadow: 'none',
-                  textDecoration: 'none',
-                  color: 'inherit',
-                }}
-                to = { '/' } > { siteTitle } 
-              </Link>
-            </h1>
-          </BackgroundImage>
-        )
-        return ( 
+  
           <div>
-            <Head />
-            { header }  
-              
+            <Header/>  
             <div 
               style = {{
                 marginLeft: 'auto',
@@ -120,7 +25,6 @@ export default ({ children, location }) => (
                 minHeight: '100vh',
               }}
             >
-              
               { children }
             </div> 
             <footer 
@@ -149,8 +53,4 @@ export default ({ children, location }) => (
               </p>
             </footer>
           </div>
-        )
-      }
-    }
-  />
 )
