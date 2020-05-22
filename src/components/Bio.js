f1a0aae9fdc1881aa226fb49d972c2e036a3c850
Alexandreffaria <alexandreffaria@me.com>
2020-05-22 10:24:48 -0300

    formatacao

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,28 +1,26 @@
 import React from 'react'
 import { rhythm } from '../utils/typography'
 
-export default ({ settings }) => ( <
-    div style = {
-        {
+export default ({ settings }) => ( 
+    <div 
+        style = {{
             display: 'flex',
             marginBottom: rhythm(2.5),
-        }
-    } >
-    <
-    img src = { settings.author_avatar.imgix_url }
-    alt = { settings.author_name }
-    style = {
-        {
-            marginRight: rhythm(1),
-            marginBottom: 0,
-            width: rhythm(5),
-            height: rhythm(5),
-        }
-    }
-    /> <
-    div dangerouslySetInnerHTML = {
-        { __html: settings.author_bio }
-    }
-    /> < /
-    div >
+        }}
+    >
+        <img 
+            src = { settings.author_avatar.imgix_url }
+            alt = { settings.author_name }
+            style = {{
+                marginRight: rhythm(1),
+                marginBottom: 0,
+                width: rhythm(5),
+                height: rhythm(5),
+            }}
+        /> 
+        <div dangerouslySetInnerHTML = {{
+                __html: settings.author_bio 
+            }}
+        />
+    </div>
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -12,145 +12,145 @@ import 'typeface-montserrat'
 import 'typeface-merriweather'
 import Head from './head.js'
 
-export default ({ children, location }) => ( <
-    StaticQuery query = { graphql `
-      query LayoutQuery {
-        cosmicjsSettings(slug: { eq: "general" }) {
-          metadata {
-            site_heading
-            homepage_hero {
-              local {
-                childImageSharp {
-                  fluid(
-                    quality: 90, 
-                    maxWidth: 1920,
-                    duotone: {
-                      highlight: "#f0960e",
-                      shadow: "#0e77f0",
-                      opacity: 61
-                    }
-                  ){
-                    ...GatsbyImageSharpFluid_withWebp
+export default ({ children, location }) => ( 
+  <StaticQuery query = { graphql `
+    query LayoutQuery {
+      cosmicjsSettings(slug: { eq: "general" }) {
+        metadata {
+          site_heading
+          homepage_hero {
+            local {
+              childImageSharp {
+                fluid(
+                  quality: 90, 
+                  maxWidth: 1920,
+                  duotone: {
+                    highlight: "#f0960e",
+                    shadow: "#0e77f0",
+                    opacity: 61
                   }
+                ){
+                  ...GatsbyImageSharpFluid_withWebp
                 }
               }
             }
-            logodda {
-              local {
-                childImageSharp{
-                  fluid(quality: 90, maxWidth: 300,){
-                    ...GatsbyImageSharpFluid_withWebp
-                  }
+          }
+          logodda {
+            local {
+              childImageSharp{
+                fluid(quality: 90, maxWidth: 300,){
+                  ...GatsbyImageSharpFluid_withWebp
                 }
               }
             }
           }
         }
       }
-    ` }
-    render = {
-        data => {
-            const siteTitle = data.cosmicjsSettings.metadata.site_heading
-            const homgePageHero = data.cosmicjsSettings.metadata.homepage_hero.local.childImageSharp.fluid
-            const logoDDA = data.cosmicjsSettings.metadata.logodda.local.childImageSharp.fluid
-            let header
-
-            let rootPath = `/`
-            let postsPath = `/posts`
-            
-            header = ( 
-              <BackgroundImage 
-                Tag = "div"
-                className = "post-hero"
-                fluid = { homgePageHero }
-                style = {{
-                  height: rhythm(24),
-                  position: 'relative',
-                  marginBottom: `${rhythm(1.1618)}`,
-                }}
-              >
-                <div 
-                  className = 'logo'
-                  style = {{
-                    maxWidth: 150,
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                    paddingTop: `${rhythm(1.1618)}`,
-                  }}
-                >
-                  <Img 
-                    fluid = { logoDDA }
-                    alt = 'Logomarca Dia do Arauto'
-                  /> 
-                </div> 
-                <h1
-                  style = {{
-                    ...scale(1.3),
-                      position: 'absolute',
-                      textAlign: 'center',
-                      left: 0,
-                      right: 0,
-                      top: rhythm(10),
-                      marginTop: '0',
-                      height: rhythm(2.5),
-                      color: 'white'
-                  }}
-                >
-                  <Link 
-                    style = {{
-                      boxShadow: 'none',
-                      textDecoration: 'none',
-                      color: 'inherit',
-                    }}
-                    to = { '/' } > { siteTitle } 
-                  </Link>
-                </h1>
-              </BackgroundImage>
-            )
-            return ( 
-              <div> 
-                <Head />
-                { header }
-                
-                <div 
-                  style = {{
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                    maxWidth: rhythm(24),
-                    padding: `0 ${rhythm(3 / 4)} ${rhythm(1.5)} ${rhythm(3 / 4)}`,
-                    minHeight: 'calc(100vh - 42px)',
-                  }}
-                >
-                  { children }
-                </div> 
-                <footer 
-                  style = {{
-                    textAlign: 'center',
-                    padding: `0 20px 80px 0`,
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                  }}
-                >
-                  <p>
-                    Desenvolvido orgulhosamente por &nbsp
-                      <a 
-                        target = "_blank"
-                        href = "https://diadoarauto.com.br"
-                        style = {{
-                          color: 'orange',
-                          boxShadow: 'none',
-                        }}
-                      >
-                        <strong>
-                          Dia do Arauto. 
-                        </strong>
-                      </a>
-                    Todos os direitos reservados © {new Date().getFullYear()}
-                  </p>
-                </footer>
-              </div>
-            )
-        }
     }
-    />
+  `}
+    render = {
+      data => {
+        const siteTitle = data.cosmicjsSettings.metadata.site_heading
+        const homgePageHero = data.cosmicjsSettings.metadata.homepage_hero.local.childImageSharp.fluid
+        const logoDDA = data.cosmicjsSettings.metadata.logodda.local.childImageSharp.fluid
+        let header
+        let rootPath = `/`
+        let postsPath = `/posts`
+            
+        header = ( 
+          <BackgroundImage 
+            Tag = "div"
+            className = "post-hero"
+            fluid = { homgePageHero }
+            style = {{
+              height: rhythm(24),
+              position: 'relative',
+              marginBottom: `${rhythm(1.1618)}`,
+            }}
+          >
+            <div 
+              className = 'logo'
+              style = {{
+                maxWidth: 150,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                paddingTop: `${rhythm(1.1618)}`,
+              }}
+            >
+              <Img 
+                fluid = { logoDDA }
+                alt = 'Logomarca Dia do Arauto'
+              /> 
+            </div> 
+            <h1
+              style = {{
+                ...scale(1.3),
+                  position: 'absolute',
+                  textAlign: 'center',
+                  left: 0,
+                  right: 0,
+                  top: rhythm(10),
+                  marginTop: '0',
+                  height: rhythm(2.5),
+                  color: 'white'
+              }}
+            >
+              <Link 
+                style = {{
+                  boxShadow: 'none',
+                  textDecoration: 'none',
+                  color: 'inherit',
+                }}
+                to = { '/' } > { siteTitle } 
+              </Link>
+            </h1>
+          </BackgroundImage>
+        )
+        return ( 
+          <div>
+            <Head />
+            { header }  
+              
+            <div 
+              style = {{
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                maxWidth: rhythm(25),
+                padding: `0 ${rhythm(3 / 4)} ${rhythm(1.5)} ${rhythm(3 / 4)}`,
+                minHeight: '100vh',
+              }}
+            >
+              
+              { children }
+            </div> 
+            <footer 
+              style = {{
+                textAlign: 'center',
+                padding: `0 20px 80px 0`,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+              }}
+            >
+              <p>
+                Desenvolvido orgulhosamente por &nbsp
+                  <a 
+                    target = "_blank"
+                    href = "https://diadoarauto.com.br"
+                    style = {{
+                      color: 'orange',
+                      boxShadow: 'none',
+                    }}
+                  >
+                    <strong>
+                      Dia do Arauto. 
+                    </strong>
+                  </a>
+                Todos os direitos reservados © {new Date().getFullYear()}
+              </p>
+            </footer>
+          </div>
+        )
+      }
+    }
+  />
 )
