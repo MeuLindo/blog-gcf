7d65612fa6bc958b5cab4304571f22c4d8c02df3
Alexandreffaria <alexandreffaria@me.com>
2020-05-22 09:58:13 -0300

    Remocao da bio na home

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,55 +6,71 @@ import { graphql } from 'gatsby'
 
 import Bio from '../components/Bio'
 import Layout from '../components/layout'
-import { rhythm } from '../utils/typography'
+import { rhythm, scale } from '../utils/typography'
 
 class BlogIndex extends React.Component {
-    render() {
-        const siteTitle = get(
-            this,
-            'props.data.cosmicjsSettings.metadata.site_title'
-        )
-        const posts = get(this, 'props.data.allCosmicjsPosts.edges')
-        const author = get(this, 'props.data.cosmicjsSettings.metadata')
-        const location = get(this, 'props.location')
-
-        return ( <
-            Layout location = { location } >
-
-            <
-            Helmet title = { siteTitle }
-            />  <
-            Bio settings = { author }
-            /> {
-                posts.map(({ node }) => {
-                    const title = get(node, 'title') || node.slug
-                    return ( <
-                        div key = { node.slug } >
-                        <
-                        h3 style = {
-                            {
-                                marginBottom: rhythm(1 / 4),
-                            }
-                        } >
-                        <
-                        Link style = {
-                            { boxShadow: 'none' }
-                        }
-                        to = { `posts/${node.slug}` } > { title } <
-                        /Link> < /
-                        h3 > <
-                        small > { node.created } < /small> <
-                        p dangerouslySetInnerHTML = {
-                            { __html: node.metadata.description }
-                        }
-                        /> < /
-                        div >
-                    )
-                })
-            } <
-            /Layout>
-        )
-    }
+  render() {
+    const siteTitle = get(
+      this,
+      'props.data.cosmicjsSettings.metadata.site_title'
+    )
+    const posts = get(this, 'props.data.allCosmicjsPosts.edges')
+    const author = get(this, 'props.data.cosmicjsSettings.metadata')
+    const location = get(this, 'props.location')
+        
+    return ( 
+      <Layout location = { location } >
+        <Helmet title = { siteTitle } />
+        {/* <Bio settings = { author }/>  */}
+        <h2 
+          style={{
+            textAlign: 'center',
+            fontSize: rhythm(1.8),
+            marginBottom: rhythm(1.8),
+          }}
+        >
+          Se divirta!
+        </h2>
+        {
+          posts.map(({ node }) => {
+            const title = get(node, 'title') || node.slug
+              return (
+                <div 
+                  key = { node.slug } 
+                >
+                  <h3 
+                    style = {{
+                      marginBottom: rhythm(0),
+                    }}
+                  >
+                    <Link 
+                      style = {{ 
+                        boxShadow: 'none' 
+                      }}
+                      to = { `posts/${node.slug}` } > { title }
+                    </Link>
+                  </h3>
+                  <div
+                    style = {{
+                      marginBottom: rhythm(.616),
+                      fontSize: rhythm(0.5)
+                    }}
+                  > 
+                    { node.created }
+                  </div>
+                  <p 
+                    dangerouslySetInnerHTML = {{
+                      __html: node.metadata.description 
+                    }}
+                  >
+                  </p>
+                </div>
+              )
+          })
+        }
+      </Layout>
+    )
+  }
 }
 
 export default BlogIndex
