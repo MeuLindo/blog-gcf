28f9b95509da063267bef64f45ee886a2f3a7013
Alexandreffaria <alexandreffaria@me.com>
2020-05-22 02:38:57 -0300

    tema funston type

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,19 +1,23 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+import funstonTheme from 'typography-theme-funston'
 
-Wordpress2016.overrideThemeStyles = () => ({
-  'a.gatsby-resp-image-link': {
-    boxShadow: 'none',
-  },
-})
+// import Wordpress2016 from 'typography-theme-wordpress-2016'
 
-delete Wordpress2016.googleFonts
+// Wordpress2016.overrideThemeStyles = () => ({
+//   'a.gatsby-resp-image-link': {
+//     boxShadow: 'none',
+//   },
+// })
 
-const typography = new Typography(Wordpress2016)
+// delete Wordpress2016.googleFonts
+
+// const typography = new Typography(Wordpress2016)
+
+const typography = new Typography(funstonTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
-  typography.injectStyles()
+    typography.injectStyles()
 }
 
 export default typography
